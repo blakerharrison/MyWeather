824598b5ce58496528e5761318ca2aef8a488f55
Blake Harrison <blakerh@protonmail.com>
2019-04-12 13:15:16 -0400

    Initial Commit

---
+++ MyWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWeather
+//
+//  Created by Blake Harrison on 4/12/19.
+//  Copyright © 2019 Blake Harrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
